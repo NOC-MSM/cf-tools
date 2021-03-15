be09b8474d9cfd575a0d98725f759de24367febf
malmans2 <mattia.almansi@noc.ac.uk>
2021-03-15 13:58:37 +0000

    use dask for CI

--- cf_tools/tests/datasets.py
+++ cf_tools/tests/datasets.py
@@ -11,9 +11,11 @@ from .conftest import data_path
 orca2_ice_pisces_path = os.path.join(data_path, "ORCA2_ICE_PISCES")
 orca2_ice_pisces = dict(
     domain_cfg=xr.open_dataset(
-        os.path.join(orca2_ice_pisces_path, "ORCA_R2_zps_domcfg.nc")
+        os.path.join(orca2_ice_pisces_path, "ORCA_R2_zps_domcfg.nc"), chunks=-1
     ),
-    mesh_mask=xr.open_dataset(os.path.join(orca2_ice_pisces_path, "mesh_mask.nc")),
+    mesh_mask=xr.open_dataset(
+        os.path.join(orca2_ice_pisces_path, "mesh_mask.nc"), chunks=-1
+    ),
     grid_T=xr.open_mfdataset(os.path.join(orca2_ice_pisces_path, "*grid_T*.nc")),
     grid_U=xr.open_mfdataset(os.path.join(orca2_ice_pisces_path, "*grid_U*.nc")),
     grid_V=xr.open_mfdataset(os.path.join(orca2_ice_pisces_path, "*grid_V*.nc")),
