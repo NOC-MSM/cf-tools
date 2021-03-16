9551722706d10f3b50bd4aafd95d6903fadd302b
malmans2 <mattia.almansi@noc.ac.uk>
2021-03-16 10:57:42 +0000

    better testing

--- cf_tools/tests/test_accessor.py
+++ cf_tools/tests/test_accessor.py
@@ -11,12 +11,12 @@ import cf_tools  # noqa: F401 pylint: disable=W0611
 def test_options():
 
     airds.cf_tools.set_options(a=1)
-    assert airds.cf_tools.options["a"] == 1
+    assert airds.cf_tools.options == dict(a=1)
 
     airds.cf_tools.unset_options("a")
     assert airds.cf_tools.options == {}
 
     with airds.cf_tools:
         airds.cf_tools.set_options(a=1)
-        assert airds.cf_tools.options["a"] == 1
+        assert airds.cf_tools.options == dict(a=1)
     assert airds.cf_tools.options == {}
